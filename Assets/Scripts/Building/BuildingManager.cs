ef8915cf1c6cc935a1233a5b487384e462ea45fe
ihavebeenjamin <42360305+ihavebeenjamin@users.noreply.github.com>
2018-11-25 14:54:20 -0600

    gooby

--- Assets/Scripts/Building/BuildingManager.cs
+++ Assets/Scripts/Building/BuildingManager.cs
@@ -2,28 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuildingManager : MonoBehaviour {
+public class BuildingManager : MonoBehaviour
+{
 
     public GameObject[] buildings;
     private BuildingPlacement buildingPlacement;
-	// Use this for initialization
-	void Start () {
-        buildingPlacement = GetComponent<BuildingPlacement>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    void OnGUI() 
+    // Use this for initialization
+    void Start()
     {
-        for(int i = 0; i <buildings.Length; i++)
-        {
-            if (GUI.Button(new Rect(Screen.width - 120 , Screen.height/20 +Screen.height/12 *i, 100, 30), buildings[i].name))
-            {
-                buildingPlacement.SetItem(buildings[i]);
-            }
-        }
+        buildingPlacement = GetComponent<BuildingPlacement>();
     }
-}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnGUI()
+    {
+        // for(int i = 0; i <buildings.Length; i++)
+        //  {
+        //  if (GUI.Button(new Rect(Screen.width - 120 , Screen.height/20 +Screen.height/12 *i, 100, 30), buildings[i].name))
+        //  {
+       // buildingPlacement.SetItem(buildings[i]);
+        //  }
+        // }
+         }
+    }
 
--- Assets/Scripts/GoblinMovement.cs
+++ Assets/Scripts/GoblinMovement.cs
@@ -1,100 +1,137 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GoblinMovement : MonoBehaviour {
+
+public class GoblinMovement : MonoBehaviour
+{
+    public float moveSpeed;
     private Animator anim;
-    private bool moving;
-    public int moveSpeed;
 
-    public int time;
-    public float CurrentTime;
- 
-
-    private Vector3 moveDirection;
     private Rigidbody2D myRigidbody;
 
+    public bool moving;
+
+    public float timeBetweenMove;
+    private float timeBetweenMoveCounter;
+
+    public float timeToMove;
+    private float timeToMoveCounter;
+
+    private int switchCase;
+
+    private Vector3 moveDirection;
+
+    public float waitToReload;
+    private bool reloading;
+    private GameObject thePlayer;
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         anim = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
-        moving = true;
+
+        //  timeBetweenMoveCounter = timeBetweenMove;
+        //  timeToMoveCounter = timeToMove;
+
+        timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
+        timeToMoveCounter = Random.Range(timeToMove * .75f, timeToMove * 1.25f);
+
     }
-	
-	// Update is called once per frame
-	void Update () {
-       // while (moving == true)
-       // {
-           
-        myRigidbody.velocity = moveDirection;
-        int caseSwitch = caseSwitch = Random.Range(0, 5);
 
-        switch (caseSwitch)
-        {
-            case 1:
-                time = Random.Range(2, 5);
-                moveUp();
-                
-                break;
-            case 2:
-                time = Random.Range(2, 5);
-                moveDown();
-                break;
-            case 3:
-                time = Random.Range(2, 5);
-                moveLeft();
-                break;
-            case 4:
-                time = Random.Range(2, 5);
-                moveRight();
-                break;
-            default:
-
-                break;
-
-
-                // }
-        }
-        }
-    void moveUp()
+    // Update is called once per frame
+    void Update()
     {
-        while (CurrentTime < time)
+       
+        if (moving)
         {
-            anim.SetFloat("yMove", 1);
-            moveDirection = new Vector3(0 * moveSpeed, 1 * moveSpeed, 0f);
-            CurrentTime = Time.deltaTime;
+            timeToMoveCounter -= Time.deltaTime;
+            myRigidbody.velocity = moveDirection;
+
+            if (timeToMoveCounter < 0f)
+            {
+                moving = false;
+                anim.SetBool("Moving", false);
+                timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
+            }
         }
-      
+        else
+        {
+            timeBetweenMoveCounter -= Time.deltaTime;
+            myRigidbody.velocity = Vector2.zero;
+
+            if (timeBetweenMoveCounter < 0f)
+            {
+                timeToMoveCounter = Random.Range(timeToMove * .75f, timeToMove * 1.25f);
+                switchCase = Random.Range(1, 5);
+                moving = true;
+                anim.SetBool("Moving", true);
+                switch (switchCase)
+                    
+                {
+                    case 1:
+                        anim.SetFloat("xMove", -1);
+                        anim.SetFloat("yMove", 0);
+                        anim.SetFloat("xLast", -1);
+                        anim.SetFloat("yLast", 0);
+                        Debug.Log("Moving Left");
+                        moveDirection = new Vector3(-1 * moveSpeed, 0, 0f);
+                        
+                        break;
+
+                    case 2:
+                        anim.SetFloat("yMove", 0);
+                        anim.SetFloat("xMove", 1);
+                        anim.SetFloat("yLast", 0);
+                        anim.SetFloat("xLast", 1);
+                        Debug.Log("Moving Right");
+                        moveDirection = new Vector3(1 * moveSpeed, 0, 0f);
+                       
+                        break;
+
+                    case 3:
+                        anim.SetFloat("xMove", 0);
+                        anim.SetFloat("yMove", 1);
+                        anim.SetFloat("xLast", 0);
+                        anim.SetFloat("yLast", 1);
+                        Debug.Log("Moving up");
+                        moveDirection = new Vector3(0, 1*moveSpeed, 0f);
+                       
+                        break;
+                    case 4:
+                        anim.SetFloat("xMove", 0);
+                        anim.SetFloat("yMove", -1);
+                        anim.SetFloat("xLast", 0);
+                        anim.SetFloat("yLast", -1);
+                        moveDirection = new Vector3(0, -1*moveSpeed, 0f);
+                        Debug.Log("Moving Down");
+
+                        Debug.Log("Movingup");
+                        break;
+                    default:
+                        anim.SetBool("Moving", false);
+                        break;
+
+
+                }
+               
+
+               // moveDirection = new Vector3(Random.Range(-1f, 1f) * moveSpeed, 0 * moveSpeed, 0f);
+            }
+
+        }
+        if (reloading)
+        {
+            waitToReload -= Time.deltaTime;
+            if (waitToReload <= 0)
+            {
+                SceneManager.LoadScene("main");
+                thePlayer.SetActive(true);
+
+            }
+        }
     }
 
-    void moveDown()
-    {
-        while (CurrentTime < time)
-        {
-            anim.SetFloat("yMove", 1);
-            moveDirection = new Vector3(0 * moveSpeed, -1 * moveSpeed, 0f);
-            CurrentTime = Time.deltaTime;
-        }
-    }
-    
-
-    void moveLeft()
-    {
-        while (CurrentTime < time)
-        {
-            anim.SetFloat("yMove", 1);
-            moveDirection = new Vector3(-1 * moveSpeed, 0 * moveSpeed, 0f);
-            CurrentTime = Time.deltaTime;
-        }
-    }
-
-    void moveRight()
-    {
-        while (CurrentTime < time)
-        {
-            anim.SetFloat("yMove", 1);
-            moveDirection = new Vector3(1 * moveSpeed, 1 * moveSpeed, 0f);
-            CurrentTime = Time.deltaTime;
-        }
-    }
 }
