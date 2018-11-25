25de1f2a24c85620947736f68b22c79ba92538ff
ElasticCracker <45251205+ElasticCracker@users.noreply.github.com>
2018-11-24 21:58:28 -0600

    BuildingPlacement

--- Assets/Scripts/Building/BuildingPlacement.cs
+++ Assets/Scripts/Building/BuildingPlacement.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class BuildingPlacement : MonoBehaviour {
 
-    private Transform currentBuilding;
+    private PlaceableBuilding placeableBuilding;
+    // private Transform currentBuilding;
     private GameObject selectedBuild;
     private bool hasPlaced;
-	// Use this for initialization
+    	// Use this for initialization
 	void Start () {
 		
 	}
@@ -17,13 +18,15 @@ public class BuildingPlacement : MonoBehaviour {
 		if (selectedBuild != null && !hasPlaced)
        {
            Vector2 m = Input.mousePosition;
-            Debug.Log(m);
+            
             selectedBuild.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
 
             if (Input.GetMouseButtonDown(0))
             {
+                Debug.Log(isLegalPosition());
                 if (isLegalPosition())
                 {
+                    
                     hasPlaced = true;
                 }
             }
@@ -32,13 +35,18 @@ public class BuildingPlacement : MonoBehaviour {
 
     bool isLegalPosition()
     {
+        
+        if (placeableBuilding.collideCounter > 0)
+        {
+          return false;
+        }
         return true;
     }
     public void SetItem(GameObject b)
     {
         hasPlaced = false;
         selectedBuild = Instantiate(b, Input.mousePosition, transform.rotation);
-        
+        placeableBuilding = selectedBuild.GetComponent<PlaceableBuilding>();
         Debug.Log(selectedBuild);
     }
 }
--- Assets/Scripts/Building/PlaceableBuilding.cs
+++ Assets/Scripts/Building/PlaceableBuilding.cs
@@ -3,21 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlaceableBuilding : MonoBehaviour {
-    [HideInInspector]
+    // [HideInInspector]
     public List<Collider> colliders = new List<Collider>();
+    public float collideCounter;
     
 	// Use this for initialization
 	void Start () {
-		
-	}
+        collideCounter = 0;
+        Debug.Log(collideCounter);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-    private void OnTriggerEnter(Collider c) {
+    void OnTriggerEnter(Collider c) {
+        if (c.tag == "Building")
+        {
+            colliders.Add(c);
+            collideCounter++;
+
+            Debug.Log(collideCounter);
+        }
     }
-    
-        
+
+    void OnTriggerExit(Collider c)
+    {
+        if (c.tag == "Building")
+        {
+            Debug.Log(collideCounter);
+            colliders.Remove(c);
+            collideCounter--;
+
+        }
     }
+
 }
+
